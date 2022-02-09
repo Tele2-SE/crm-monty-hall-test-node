670d30349d310790fd22001b40676febb851cdf1
Edvin Berg <edvinberg@hotmail.com>
2022-02-09 18:33:42 +0100

    fix comment

--- server/montyHallSimulation.js
+++ server/montyHallSimulation.js
@@ -2,12 +2,12 @@ const simulate1 = (switchDoor) => {
   let doorWithPrize = 0;
   let guess = Math.floor(Math.random() * 3);
 
-  //If you select door with prize you only win if you choose to not switch door
+  //If you select the door with prize you only win if you choose to not switch door
   if (guess === doorWithPrize) {
     return !switchDoor;
   }
 
-  //If you select door without prize you only win if you choose to switch door
+  //If you select a door without prize you only win if you choose to switch door
   return switchDoor;
 }
 
