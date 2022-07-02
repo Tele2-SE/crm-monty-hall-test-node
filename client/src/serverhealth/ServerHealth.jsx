dc66875d2c28b00914baa2c69b7c9766862cb550
Jonn Nilsson <jonn@jonn.nu>
2022-07-02 08:52:17 +0200

    fix: code cleanup

--- client/src/serverhealth/ServerHealth.jsx
+++ client/src/serverhealth/ServerHealth.jsx
@@ -3,15 +3,13 @@ import './ServerHealth.css';
 import useServerHealth from './useServerHealth';
 
 const ServerHealth = () => {
- const serverHealth  = useServerHealth();
+  const serverHealth  = useServerHealth();
 
-    return (
-      <div className="serverHealth">
-        <p>Backend is: <a className={serverHealth}>{serverHealth}</a></p>
-      </div>
-    );
-
-
+  return (
+    <div className="serverHealth">
+      <p>Backend is: <a className={serverHealth}>{serverHealth}</a></p>
+    </div>
+  );
 }
 
 export default ServerHealth;
